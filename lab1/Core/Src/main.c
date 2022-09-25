9afb7c54ed9bc51dd87b777ac86a8a4d808c6df2
tien2032002 <tien.nguyentien203@hcmut.edu.vn>
2022-09-25 10:20:34 +0700

    lab1.1

--- lab1/Core/Src/main.c
+++ lab1/Core/Src/main.c
@@ -93,6 +93,8 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+	  HAL_GPIO_TogglePin(RED_LED_GPIO_Port, RED_LED_Pin);
+	  HAL_Delay(1000);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
